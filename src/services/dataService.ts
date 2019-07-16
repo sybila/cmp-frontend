6a14569875fd5a5a54a82c24b27a28c7c41565a8
Martin Petr <martinpetr@localhost.localdomain>
2019-07-17 00:16:26 +0200

    Access token interceptor

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -3,13 +3,25 @@ import axios from "axios";
 import Config from "../config";
 const url = Config.apiDomain;
 
-/**
- * TODO: Create custom interceptors for authentication
- */
-
 const dataService = axios.create({
   baseURL: url,
   timeout: 10000
 });
+
+/**
+ * Interceptor which includes OAuth token in case it is stored in Local Storage
+ */
+dataService.interceptors.request.use(
+  (config: any) => {
+    const token = JSON.parse(localStorage.getItem("user") as string);
+
+    if (token !== null) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error: any) => Promise.reject(error)
+);
 
 export default dataService;
