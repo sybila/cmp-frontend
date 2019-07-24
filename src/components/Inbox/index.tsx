6f84334d0fcc2e80bdac366e4b2f8130c0f46403
Martin Petr <martinpetr@localhost.localdomain>
2019-07-25 01:03:47 +0200

    Inbox toggling (click outside event)

--- src/components/Inbox/index.tsx
+++ src/components/Inbox/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { Dispatch, bindActionCreators } from "redux";
 import { CSSTransition } from "react-transition-group";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import { AppState } from "../../reducers/globalReducer";
 import {
@@ -9,32 +11,61 @@ import {
   getNotifications
 } from "../../selectors/notificationsSelectors";
 import { NotificationModel } from "../../models/Notification";
-import { loadNotifications } from "../../actions/notificationActions";
+import {
+  loadNotifications,
+  toggleInbox
+} from "../../actions/notificationActions";
 import InboxItem from "./InboxItem";
 
 interface Props {
   isOpen: boolean;
   loadNotifications: (id: number) => any;
+  toggleInbox: () => void;
   notifications: NotificationModel[];
 }
 
 interface State {}
 
 class Inbox extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.setInboxRef = this.setInboxRef.bind(this);
+  }
+
   componentDidMount() {
     const { loadNotifications } = this.props;
     loadNotifications(99);
+
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  private inboxRef: any;
+
+  setInboxRef(node: any) {
+    this.inboxRef = node;
+  }
+
+  handleClickOutside(event: any) {
+    const { isOpen } = this.props;
+    if (this.inboxRef && !this.inboxRef.contains(event.target) && isOpen)
+      this.props.toggleInbox();
   }
 
   render() {
-    const { isOpen, notifications } = this.props;
+    const { isOpen, notifications, toggleInbox } = this.props;
 
     const nodes = notifications.map(item => {
       return <InboxItem title={"Test"} text={item.message} />;
     });
 
     return (
-      <div>
+      <div ref={this.setInboxRef}>
         <CSSTransition
           in={isOpen}
           timeout={300}
@@ -42,7 +73,15 @@ class Inbox extends React.Component<Props, State> {
           unmountOnExit
         >
           <div className={"inbox"}>
-            <h3>Notifications</h3>
+            <div className={"inbox-heading"}>
+              <h3>Notifications</h3>
+              <div
+                className={"inbox-close"}
+                onClick={() => isOpen && toggleInbox()}
+              >
+                <FontAwesomeIcon icon={faTimes} />
+              </div>
+            </div>
             <div className={"inbox-wrapper"}>{nodes}</div>
           </div>
         </CSSTransition>
@@ -57,7 +96,8 @@ const mapStateToProps = (state: AppState) => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  loadNotifications: bindActionCreators(loadNotifications, dispatch)
+  loadNotifications: bindActionCreators(loadNotifications, dispatch),
+  toggleInbox: bindActionCreators(toggleInbox, dispatch)
 });
 
 export default connect(
