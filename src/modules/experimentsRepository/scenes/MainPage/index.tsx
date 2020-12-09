29435d1b3667a1f894609633358fd491a08ed94e
Martin Petr <martin.petr.276@gmail.com>
2020-12-09 13:25:24 +0100

    Models module info

--- src/modules/experimentsRepository/scenes/MainPage/index.tsx
+++ src/modules/experimentsRepository/scenes/MainPage/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { moduleNames as experimentsNames } from "../../reducers/MainReducer";
 
 interface Props {}
 
@@ -9,26 +11,33 @@ interface State {}
 class MainPage extends React.Component<Props, State> {
   render() {
     return (
-      <div className="section">
-        <div className="container">
-          <h2 className="title is-2">Experiments</h2>
-          <div className="box">
-            <p>
-              Model repository contains computational models of selected
-              biological processes relevant for cyanobacteria. Models
-              implemented on this website are manually curated, integrated
-              within the e-cyanobacterium formal Biochemical Space, and
-              associated with cross-references.{" "}
-            </p>
-
-            <p>
-              Most of the implemented models are already published, though some
-              of the models present fresh work which might be yet unpublished.
-              All of the models are available in public domain.
-            </p>
+      <>
+        <BreadcrumbsItem to={`/${experimentsNames.url}/info`}>
+          About experiments
+        </BreadcrumbsItem>
+        <div className="section">
+          <div className="container">
+            <h2 className="title is-2">About experiments module</h2>
+            <div className="box">
+              <p>
+                Main purpose of experiments in CMP is model validation.
+                Experiment repository is a tool for storage and presentation of
+                time-series data from wet-lab experiments. Every experiment
+                should be well described (device, medium, organism, etc.) and
+                annotated, so that will be possible to reproduce the result.
+                Experiment's variables might be related to Biochemical Space and
+                should have a set unit. Measured data can be imported from
+                specific formats and you can export the experiment to your local
+                device. There is also an option to add notes to experiment or
+                specific variables. Notes can be plain text or picture and are
+                assigned to particular time. Moreover, it is possible to
+                visualise the data in a table or chart, where you can compare
+                experiment data with models simulations.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
--- src/modules/modelsRepository/scenes/MainPage/index.tsx
+++ src/modules/modelsRepository/scenes/MainPage/index.tsx
@@ -1,43 +1,75 @@
 import React from "react";
+import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { moduleNames as modelsNames } from "../../reducers/MainReducer";
 
-interface Props { }
+interface Props {}
 
-interface State { }
+interface State {}
 
 class MainPage extends React.Component<Props, State> {
-    render() {
-        return (
-            <div className="section">
-                <div className="container">
-                    <h2 className="title is-2">Models repository</h2>
-                    <div className="box">
-                        <p>
-                            Model repository contains computational models of selected
-                            biological processes relevant for cyanobacteria. Models
-                            implemented on this website are manually curated, integrated
-                            within the e-cyanobacterium formal Biochemical Space, and
-                            associated with cross-references.{" "}
-                        </p>
+  render() {
+    return (
+      <>
+        <BreadcrumbsItem to={`/${modelsNames.url}/info`}>
+          About models
+        </BreadcrumbsItem>
+        <div className="section">
+          <div className="container">
+            <h2 className="title is-2">About models module</h2>
+            <div className="box">
+              <p>
+                The model module provides an interface to create, store,
+                simulate and analyze the computational models relevant in the
+                given CMP context. CMP model repository supports similar types
+                of models as models that can be written in the SBML standard,
+                since our database reflects the structure of SBML. This means
+                that following entities can be defined and be added to any
+                model:
+                <ul className={"classic"}>
+                  <li>compartment</li>
+                  <li>reaction</li>
+                  <li>specie</li>
+                  <li>reaction item</li>
+                  <li>function</li>
+                  <li>constraint</li>
+                  <li>event</li>
+                  <li>event assignment</li>
+                  <li>initial assignment</li>
+                  <li>function definition</li>
+                  <li>rule</li>
+                </ul>
+              </p>
 
-                        <p>
-                            Most of the implemented models are already published, though some
-                            of the models present fresh work which might be yet unpublished.
-                            All of the models are available in public domain.
-                        </p>
-                    </div>
-                </div>
+              <p>
+                Some of these entities can be mapped to BCS. This backs up the
+                informational value of the platform. Such mappings serve for
+                easier determination of model’s or experiment’s overlaps and
+                connections with each other, including the easier revelation of
+                their possible meaning.
+              </p>
+
+              <p>
+                The module also provides an import and export of SBML documents.
+              </p>
+
+              <p>
+                Attentive readers could point out that units and units
+                definitions are too a part of SBML model. We’ve covered that
+                functionality in our Units module, that serves both, Model and
+                Experiment modules, for convenience.
+              </p>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({});
 
-const mapStateToProps = state => ({});
+const mapStateToProps = (state) => ({});
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(MainPage);
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
