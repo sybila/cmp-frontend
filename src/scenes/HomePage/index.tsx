0e689b34255cf5bd77c7e7f6e125a2ed4fadb675
Martin Petr <martin.petr.276@gmail.com>
2020-12-09 13:14:58 +0100

    Home page update

--- src/scenes/HomePage/index.tsx
+++ src/scenes/HomePage/index.tsx
@@ -7,6 +7,8 @@ import { getUser } from "ApplicationSelectors";
 import { AppState } from "reducers/GlobalReducer";
 import Dashboard from "modules/administration/scenes/Dashboard";
 
+import HeroImg from "assets/cmp-hero-img.png";
+
 interface Props {
   user: UserModel;
 }
@@ -16,17 +18,36 @@ class LoginPage extends React.Component<Props> {
 
   render() {
     return !this.props.user ? (
-      <div className={"landing-hero section"}>
-        <div className="container">
-          <h1>Lorem ipsum dolor sit</h1>
+      <div className={"landing-hero section container"}>
+        <figure className="image">
+          <img
+            src={HeroImg}
+            title="Comprehensive Modelling Platform"
+            alt="Comprehensive Modelling Platform"
+          />
+        </figure>
+        <div>
+          <h1>Comprehensive Modelling Platform</h1>
           <p className={"lead"}>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent
-            id justo in neque elementum ultrices. Fusce dui leo, imperdiet in,
-            aliquam sit amet, feugiat eu, orci. Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur.
+            CMP is a general framework for public sharing, annotation, and
+            visualisation of domain-specific dynamical models and wet-lab
+            experiments.
           </p>
-          <a className={"btn btn-primary btn-lg"} href="/" role="button">
+          <p>
+            The platform is unique in integrating abstract mathematical models
+            with a precise consortium-agreed bio-chemical description provided
+            in a rule-based formalism. The general aim is to stimulate
+            collaboration between experimental and computational systems
+            biologists to achieve a better understanding of the domain-specific
+            system.
+          </p>
+          <p>
+            CMP is logically divided into 4 parts. Each of them has a specific
+            responsibility and works with a different database. Modules
+            communicate with each other in specific ways concerning their
+            competence defined by their functionalities.
+          </p>
+          <a className={"btn btn-primary btn-lg cta"} href="/" role="button">
             Learn more
           </a>
         </div>
