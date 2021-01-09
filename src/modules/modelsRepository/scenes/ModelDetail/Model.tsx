c84980e212f3355937a8eea1a4156ee03b6d2c71
Martin Petr <martin.petr.276@gmail.com>
2021-01-10 00:29:27 +0100

    Add author to model detail

--- src/modules/modelsRepository/scenes/ModelDetail/Model.tsx
+++ src/modules/modelsRepository/scenes/ModelDetail/Model.tsx
@@ -1,8 +1,11 @@
+import { useApi } from "hooks/useApi";
 import { Model as ModelInterface } from "models/Model";
 import { getModelById } from "modules/modelsRepository/selectors";
-import React from "react";
+import React, { useCallback } from "react";
+import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { useSelector } from "react-redux";
 import { useRouteMatch } from "react-router-dom";
+import api from "services/api";
 
 interface Props {
   description?: string;
@@ -13,12 +16,20 @@ const Model = (props: Props) => {
   const getModel = useSelector(getModelById);
 
   const model: ModelInterface = getModel(parseInt(match.params.modelId, 10));
+  const [author] = useApi(
+    useCallback(() => api.users.getUser(model.userId), [model])
+  );
 
   return (
     <>
       <section className="section p-b-0">
         <div className="container">
           <div className="box ">
+            {author && (
+              <p>
+                Author: {author.name} {author.surname}
+              </p>
+            )}
             <p>{model.description}</p>
             <p dangerouslySetInnerHTML={{ __html: model.notes }} />
           </div>
