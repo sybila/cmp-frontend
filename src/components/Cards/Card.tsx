12b5ceb6ffbce6632d673f63855bb5c7b9b72231
Martin Petr <martin.petr.276@gmail.com>
2021-01-10 01:49:22 +0100

    Update Card component with header

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,26 +1,54 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
+type Title = string | JSX.Element;
 interface CardInterface {
-  title?: string;
-  subtitle?: string;
+  headerTitle?: Title;
+  title?: Title;
+  subtitle?: Title;
   footer?: JSX.Element[];
+  children?: React.ReactNode;
 }
 
-const Card = ({ title, subtitle, footer }: CardInterface) => {
+const Card = ({
+  title,
+  subtitle,
+  footer,
+  headerTitle,
+  children,
+}: CardInterface) => {
+  const [isOpen, toggle] = React.useState(false);
+
   return (
     <div className="card">
-      <div className="card-content">
-        {title && <p className="title">{title}</p>}
-        {subtitle && <p className="subtitle">{subtitle}</p>}
-      </div>
-      {footer && (
-        <footer className="card-footer">
-          {footer.map((footerItem) => (
-            <p className="card-footer-item">
-              <span>{footerItem}</span>
-            </p>
-          ))}
-        </footer>
+      {headerTitle && (
+        <header className="card-header" onClick={() => toggle(!isOpen)}>
+          <p className="card-header-title m-b-0">{headerTitle}</p>
+          <a href="#" className="card-header-icon" aria-label="more options">
+            <span className="icon">
+              <FontAwesomeIcon icon={isOpen ? faAngleDown : faAngleUp} />
+            </span>
+          </a>
+        </header>
+      )}
+      {(isOpen || !headerTitle) && (
+        <>
+          <div className="card-content">
+            {title && <p className="title">{title}</p>}
+            {subtitle && <p className="subtitle">{subtitle}</p>}
+            {children}
+          </div>
+          {footer && (
+            <footer className="card-footer">
+              {footer.map((footerItem) => (
+                <p className="card-footer-item">
+                  <span>{footerItem}</span>
+                </p>
+              ))}
+            </footer>
+          )}
+        </>
       )}
     </div>
   );
