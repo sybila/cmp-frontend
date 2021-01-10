df73d9331f5bf396034154921287dd9558357462
Martin Petr <martin.petr.276@gmail.com>
2021-01-10 01:50:02 +0100

    Add compartment detail

--- src/modules/modelsRepository/services.ts
+++ src/modules/modelsRepository/services.ts
@@ -1,12 +1,13 @@
 import { AxiosPromise } from "axios";
 import { ApiResponse } from "models/GenericTypes";
-import { ModelCompartment } from "models/Model";
+import { ModelCompartment, ModelCompartmentExtended } from "models/Model";
 import dataService from "services/dataService";
 
 const service = {
   load,
   loadModel,
   loadCompartments,
+  loadCompartmentDetail,
 };
 
 // REVIEW: Refactor based on API
@@ -22,6 +23,13 @@ function loadCompartments(
   modelId: number
 ): AxiosPromise<ApiResponse<ModelCompartment[]>> {
   return dataService.get(`/models/${modelId}/compartments`);
+}
+
+function loadCompartmentDetail(
+  modelId: number | string,
+  compartmentId: number | string
+): AxiosPromise<ApiResponse<ModelCompartmentExtended>> {
+  return dataService.get(`/models/${modelId}/compartments/${compartmentId}`);
 }
 
 export default service;
