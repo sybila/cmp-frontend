b55ba7cf1b2638f803735fec8be5f3b43c27aa1b
Martin Petr <martin.petr.276@gmail.com>
2021-05-04 23:22:51 +0200

    feat: add Yup validation

--- src/utils/formValidators/index.ts
+++ src/utils/formValidators/index.ts
@@ -1,7 +1,22 @@
+import { setIn } from "final-form";
 import basic from "./basic";
+import * as Yup from "yup";
 
 const composeValidators = (...validators) => (value) =>
   validators.reduce((error, validator) => error || validator(value), undefined);
+
+// To be passed to React Final Form
+export const validateFormValues = (schema: Yup.AnySchema) => async (values) => {
+  try {
+    await schema.validate(values, { abortEarly: false });
+  } catch (err) {
+    const errors = err.inner.reduce((formError, innerError) => {
+      return setIn(formError, innerError.path, innerError.message);
+    }, {});
+
+    return errors;
+  }
+};
 
 export default {
   composeValidators,
