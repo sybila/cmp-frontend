be6c7511c22f9ed5eb916ff45a8ed15849b938a9
Martin Petr <martin.petr.276@gmail.com>
2021-06-01 01:16:07 +0200

    feat: rework expression detail component

--- src/components/ExpressionDetail.tsx
+++ src/components/ExpressionDetail.tsx
@@ -10,6 +10,51 @@ import {
 import { ExpressionDetail as ExpressionDetailType } from "../models/Model";
 import { Expression } from "../models/GenericTypes";
 
+type DetailsTableProps = {
+  name: string;
+  valueCaption: string;
+  valueAccessor: string;
+  detail: Record<string, any>;
+};
+
+const DetailsTable = ({
+  name,
+  valueCaption,
+  valueAccessor,
+  detail,
+}: DetailsTableProps) => {
+  const keys = Object.keys(detail);
+
+  if (!keys.length) return null;
+
+  return (
+    <Box mb={16}>
+      <Text fontWeight="bold" mb={2}>
+        {name}
+      </Text>
+      <Table>
+        <TableSection as="thead">
+          <TableRow>
+            <TableDataCell as="th">Alias</TableDataCell>
+            <TableDataCell as="th">{valueCaption}</TableDataCell>
+          </TableRow>
+        </TableSection>
+        <TableSection as="tbody">
+          {keys.map((key) => {
+            const parameter = detail[key];
+            return (
+              <TableRow>
+                <TableDataCell>{parameter.alias}</TableDataCell>
+                <TableDataCell>{parameter[valueAccessor]}</TableDataCell>
+              </TableRow>
+            );
+          })}
+        </TableSection>
+      </Table>
+    </Box>
+  );
+};
+
 type Props = {
   name: string;
   expression: Expression<ExpressionDetailType>;
@@ -33,83 +78,24 @@ const ExpressionDetail = ({ expression, name }: Props) => {
           )}
         </LatexWrapper>
       </Box>
-      <Box mb={16}>
-        <Text fontWeight="bold" mb={2}>
-          Parameters
-        </Text>
-        <Table>
-          <TableSection as="thead">
-            <TableRow>
-              <TableDataCell as="th">Alias</TableDataCell>
-              <TableDataCell as="th">Value</TableDataCell>
-            </TableRow>
-          </TableSection>
-          <TableSection as="tbody">
-            {Object.keys(expression.detail.components.parametes).map(
-              (parameterKey) => {
-                const parameter =
-                  expression.detail.components.parametes[parameterKey];
-                return (
-                  <TableRow>
-                    <TableDataCell>{parameter.alias}</TableDataCell>
-                    <TableDataCell>{parameter.value}</TableDataCell>
-                  </TableRow>
-                );
-              }
-            )}
-          </TableSection>
-        </Table>
-      </Box>
-      <Box mb={16}>
-        <Text fontWeight="bold" mb={2}>
-          Compartments
-        </Text>
-        <Table>
-          <TableSection as="thead">
-            <TableRow>
-              <TableDataCell as="th">Alias</TableDataCell>
-              <TableDataCell as="th">Size</TableDataCell>
-            </TableRow>
-          </TableSection>
-          <TableSection as="tbody">
-            {Object.keys(expression.detail.components.compartments).map(
-              (key) => {
-                const item = expression.detail.components.compartments[key];
-                return (
-                  <TableRow>
-                    <TableDataCell>{item.alias}</TableDataCell>
-                    <TableDataCell>{item.size}</TableDataCell>
-                  </TableRow>
-                );
-              }
-            )}
-          </TableSection>
-        </Table>
-      </Box>
-      <Box mb={16}>
-        <Text fontWeight="bold" mb={2}>
-          Species
-        </Text>
-        <Table>
-          <TableSection as="thead">
-            <TableRow>
-              <TableDataCell as="th">Alias</TableDataCell>
-              <TableDataCell as="th">Size</TableDataCell>
-            </TableRow>
-          </TableSection>
-          <TableSection as="tbody">
-            {Object.keys(expression.detail.components.species).map((key) => {
-              const item = expression.detail.components.species[key];
-              return (
-                <TableRow>
-                  <TableDataCell>{item.alias}</TableDataCell>
-                  <TableDataCell>{item["initial amount"]}</TableDataCell>
-                </TableRow>
-              );
-            })}
-          </TableSection>
-        </Table>
-      </Box>
+      <DetailsTable
+        name="Parameters"
+        valueAccessor="value"
+        valueCaption="Value"
+        detail={expression.detail.components.parametes}
+      />
+      <DetailsTable
+        name="Compartments"
+        valueAccessor="size"
+        valueCaption="Size"
+        detail={expression.detail.components.compartments}
+      />
+      <DetailsTable
+        name="Species"
+        valueAccessor="initial amount"
+        valueCaption="Size"
+        detail={expression.detail.components.species}
+      />
     </Flex>
   );
 };
