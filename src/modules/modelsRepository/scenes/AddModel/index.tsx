fb5731136ecdb665f737d97084d2981a76b5dbfc
Martin Petr <martin.petr.276@gmail.com>
2021-06-16 16:50:47 +0200

    feat: show XML

--- src/modules/modelsRepository/scenes/AddModel/index.tsx
+++ src/modules/modelsRepository/scenes/AddModel/index.tsx
@@ -1,16 +1,52 @@
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import React, { useCallback, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
+import { Button, Flex } from "rebass/styled-components";
 import { moduleNames as modelsNames } from "../../../modelsRepository/reducers/MainReducer";
 import api from "../../services";
+import styled, { css } from "styled-components/macro";
+import { rem } from "polished";
+import WhiteBox from "components/WhiteBox";
+import XMLViewer from "react-xml-viewer";
+
+const UploadButton = styled(Button)(
+  ({ theme }) => css`
+    padding: ${rem(theme.custom.sizes["size-1"])}
+      ${rem(theme.custom.sizes["size-2"])};
+    margin-bottom: ${rem(theme.custom.sizes["size-1"])};
+    margin-left: ${rem(theme.custom.sizes["size-3"])};
+  `
+);
+
+const XMLBox = styled(WhiteBox)`
+  overflow: auto;
+`;
+
+const enc = new TextDecoder("utf-8");
 
 const AddModel = () => {
   const [selectedFile, setSelectedFile] = useState<File | undefined>();
   const [rawImport, setRawImport] =
     useState<string | ArrayBuffer | undefined>();
+  const [uploadStatus, setUploadStatus] = useState<string>();
 
   const handleFileUpload = useCallback(() => {
-    api.importModel(rawImport);
+    api
+      .importModel(rawImport)
+      .then(() => {
+        setSelectedFile(undefined);
+        setRawImport(undefined);
+        setUploadStatus("Uploaded successfully");
+      })
+      .catch(() => setUploadStatus("Upload failed"));
   }, [rawImport]);
+
+  const handleFileChange: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback((e) => {
+      setSelectedFile(e.target.files[0]);
+      setUploadStatus("");
+    }, []);
 
   useEffect(() => {
     if (selectedFile) {
@@ -31,20 +67,44 @@ const AddModel = () => {
       <div className="section">
         <div className="container">
           <h2>Import SBML v3 model</h2>
-          <div>
-            <input
-              type="file"
-              onChange={(e) => setSelectedFile(e.target.files[0])}
-            />
-            <button disabled={!rawImport} onClick={handleFileUpload}>
-              Upload
-            </button>
-          </div>
-          {!selectedFile && (
-            <div>
-              <br />
-              <h4>Choose the file before Uploading</h4>
-            </div>
+          <WhiteBox $isRow>
+            <Flex>
+              <div className={`file${selectedFile ? " has-file" : ""}`}>
+                <label className="file-label">
+                  <input
+                    className="file-input"
+                    type="file"
+                    onChange={handleFileChange}
+                  />
+                  <span className="file-cta">
+                    <span className="file-icon">
+                      <FontAwesomeIcon className="m-r-10" icon={faUpload} />
+                    </span>
+                    <span className="file-label">Choose a file…</span>
+                  </span>
+                  {selectedFile && (
+                    <span className="file-name">{selectedFile.name}</span>
+                  )}
+                </label>
+              </div>
+              <UploadButton
+                type="button"
+                disabled={!rawImport}
+                onClick={handleFileUpload}
+              >
+                Upload
+              </UploadButton>
+            </Flex>
+          </WhiteBox>
+          {rawImport && (
+            <XMLBox $isRow>
+              <XMLViewer xml={enc.decode(rawImport as BufferSource)} />
+            </XMLBox>
+          )}
+          {uploadStatus && (
+            <article className="message is-dark">
+              <div className="message-body">{uploadStatus}</div>
+            </article>
           )}
         </div>
       </div>
